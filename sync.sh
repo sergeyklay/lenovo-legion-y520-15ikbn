3496bab7302fb31a0f568d22add1d60d0c6fd415
Serghei Iakovlev <egrep@protonmail.ch>
2020-07-17 11:43:06 +0300

    Added portage.use configuration

--- sync.sh
+++ sync.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 rsync -a /etc/modules-load.d/ "$(pwd)/etc/modules-load.d/"
-cp /etc/portage/make.conf "$(pwd)/etc/portage/make.conf"
+
+rsync -a /etc/portage/package.use/ "$(pwd)/etc/portage/package.use/"
+cp /etc/portage/make.conf          "$(pwd)/etc/portage/make.conf"
+
 cp "/usr/src/linux-$(uname -r)/.config" "$(pwd)/usr/src/linux/.config"
